e19640c8adcdef105c054963dd29a3ebcddad8db
Darijan Mićić <darijan.micic10@gmail.com>
2024-08-26 22:13:34 +0200

    chore: create console app targeting .net 8 fw

---
+++ FIBA_OT_sim/FIBA_OT_sim/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FIBA_OT_sim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
