8dc943ee20e2ee15c1ec5998cc2133323f89efc3
Darijan Mićić <darijan.micic10@gmail.com>
2024-08-27 03:13:42 +0200

    feat: create group stage entity
    Also, I stored groups in "GroupStage" class and national teams in "Group" class as elements of a list. This improves software reliability in case the Olympic Committee and FIBA decide to revert back to the previous format (group stage consisting of 2 groups with 6 teams in each group).

---
+++ FIBA_OT_sim/FIBA_OT_sim/Model/GroupPhase.cs
@@ -0,0 +1,28 @@
+﻿namespace FIBA_OT_sim.Model
+{
+    public class GroupPhase
+    {
+        private IList<Group> groups;
+
+        public GroupPhase()
+        {
+            groups = [];
+        }
+
+        public GroupPhase(IList<Group> groups)
+        {
+            this.groups = groups;
+        }
+
+        public GroupPhase(GroupPhase groupPhase)
+        {
+            groups = groupPhase.groups;
+        }
+
+        public IList<Group> Groups
+        {
+            get { return groups; }
+            set { groups = value; }
+        }
+    }
+}
