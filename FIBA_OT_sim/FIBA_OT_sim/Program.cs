e3d346ad00efffc34a7aabfa129a14b8a4ea1344
Darijan Mićić <darijan.micic10@gmail.com>
2024-08-28 03:22:06 +0200

    feat: schedule group phase matches

--- FIBA_OT_sim/FIBA_OT_sim/Program.cs
+++ FIBA_OT_sim/FIBA_OT_sim/Program.cs
@@ -4,6 +4,10 @@ namespace FIBA_OT_sim
 {
     public class Program
     {
+        public const int TOTAL_NUMBER_OF_TEAMS = 12;
+        public const int NUMBER_OF_GROUPS = 3;
+        public const int NUMBER_OF_TEAMS_IN_EACH_GROUP = 4;
+
         public static void Main(string[] args)
         {
             GroupPhaseService groupPhaseService = new GroupPhaseService();
--- FIBA_OT_sim/FIBA_OT_sim/Services/GroupPhaseService.cs
+++ FIBA_OT_sim/FIBA_OT_sim/Services/GroupPhaseService.cs
@@ -1,4 +1,5 @@
-﻿using FIBA_OT_sim.Repositories;
+﻿using FIBA_OT_sim.Model;
+using FIBA_OT_sim.Repositories;
 
 namespace FIBA_OT_sim.Services
 {
@@ -20,6 +21,48 @@ namespace FIBA_OT_sim.Services
         public void LoadGroupPhaseFromFileSystem(string filePath)
         {
             groupPhaseRepository.LoadGroupPhaseFromFileSystem(filePath);
+        }
+
+        public void ScheduleGroupPhaseMatches()
+        {
+            foreach (Group group in groupPhaseRepository.GroupPhase.Groups)
+            {
+                Match match1 = new Match(TournamentPhaseOfMatch.FIRST_ROUND_OF_GROUP_PHASE, 
+                    group.Teams[0], group.Teams[3], new MatchResult());
+                group.Matches.Add(match1);
+                group.Teams[0].Matches.Add(match1);
+                group.Teams[3].Matches.Add(match1);
+
+                Match match2 = new Match(TournamentPhaseOfMatch.FIRST_ROUND_OF_GROUP_PHASE, 
+                    group.Teams[2], group.Teams[1], new MatchResult());
+                group.Matches.Add(match2);
+                group.Teams[2].Matches.Add(match2);
+                group.Teams[1].Matches.Add(match2);
+
+                Match match3 = new Match(TournamentPhaseOfMatch.SECOND_ROUND_OF_GROUP_PHASE, 
+                    group.Teams[1], group.Teams[0], new MatchResult());
+                group.Matches.Add(match3);
+                group.Teams[1].Matches.Add(match3);
+                group.Teams[0].Matches.Add(match3);
+
+                Match match4 = new Match(TournamentPhaseOfMatch.SECOND_ROUND_OF_GROUP_PHASE, 
+                    group.Teams[3], group.Teams[2], new MatchResult());
+                group.Matches.Add(match4);
+                group.Teams[3].Matches.Add(match4);
+                group.Teams[2].Matches.Add(match4);
+
+                Match match5 = new Match(TournamentPhaseOfMatch.THIRD_ROUND_OF_GROUP_PHASE, 
+                    group.Teams[0], group.Teams[2], new MatchResult());
+                group.Matches.Add(match5);
+                group.Teams[0].Matches.Add(match5);
+                group.Teams[2].Matches.Add(match5);
+
+                Match match6 = new Match(TournamentPhaseOfMatch.THIRD_ROUND_OF_GROUP_PHASE, 
+                    group.Teams[1], group.Teams[3], new MatchResult());
+                group.Matches.Add(match6);
+                group.Teams[1].Matches.Add(match6);
+                group.Teams[3].Matches.Add(match6);
+            }
         }
     }
 }
