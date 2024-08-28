e86b4608738362b2a0b89461486e8e41670e00f4
Darijan Mićić <darijan.micic10@gmail.com>
2024-08-29 01:35:27 +0200

    feat: implement primitive match results simulation
    The win probability of the national team is directly corelated to its FIBA ranking. Steps of the process:
    1) I separated possible FIBA ranking differences into 10 tiers. Each tier corresponds to a win probability between 0.5 and 0.95, with a 0.05 step.
    2) The random number generator is used to generate a random probability (double value from range [0, 1)).
    3) If random probability "falls inside the range" of win probability of team with higher FIBA ranking (former is smaller than latter), the team with higher FIBA ranking will win. Otherwise, the team with lower FIBA ranking will win. Win margin is equal to FIBA ranking difference.

--- FIBA_OT_sim/FIBA_OT_sim/Services/GroupPhaseService.cs
+++ FIBA_OT_sim/FIBA_OT_sim/Services/GroupPhaseService.cs
@@ -64,5 +64,102 @@ namespace FIBA_OT_sim.Services
                 group.Teams[3].Matches.Add(match6);
             }
         }
+
+        public void SimulateResultsOfGroupPhaseMatches()
+        {
+            foreach (Group group in groupPhaseRepository.GroupPhase.Groups)
+            {
+                foreach (Match match in group.Matches)
+                {
+                    int fibaRankingOfHomeTeam = match.HomeTeam.FIBARanking;
+                    Console.WriteLine("\nHome team                           FIBA ranking: " + fibaRankingOfHomeTeam);
+                    int fibaRankingOfGuestTeam = match.GuestTeam.FIBARanking;
+                    Console.WriteLine("Guest team                          FIBA ranking: " + fibaRankingOfGuestTeam);
+                    bool homeTeamHigherRanked = false;
+                    int fibaRankingDifference = fibaRankingOfHomeTeam - fibaRankingOfGuestTeam;
+                    if (fibaRankingDifference < 0)
+                    {
+                        fibaRankingDifference *= -1;
+                        homeTeamHigherRanked = true;
+                    }
+                    Console.WriteLine("Is home team                       higher ranked: " + homeTeamHigherRanked);
+                    Console.WriteLine("FIBA ranking                          difference: " + fibaRankingDifference);
+
+                    double winProbabilityOfTeamWithHigherFIBARanking = 0.0;
+                    if (fibaRankingDifference == 1)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.5;
+                    }
+                    else if (fibaRankingDifference == 2)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.55;
+                    }
+                    else if (fibaRankingDifference == 3)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.6;
+                    }
+                    else if (fibaRankingDifference == 4)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.65;
+                    }
+                    else if (fibaRankingDifference == 5)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.7;
+                    }
+                    else if (fibaRankingDifference == 6)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.75;
+                    }
+                    else if (fibaRankingDifference == 7)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.8;
+                    }
+                    else if (fibaRankingDifference == 8)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.85;
+                    }
+                    else if (fibaRankingDifference == 9)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.9;
+                    }
+                    else if (fibaRankingDifference >= 10)
+                    {
+                        winProbabilityOfTeamWithHigherFIBARanking = 0.95;
+                    }
+                    Console.WriteLine("Win probability of team with higher FIBA ranking: " 
+                        + winProbabilityOfTeamWithHigherFIBARanking);
+
+                    // 40 : 48 = avg_in_fiba : avg_in_nba
+                    // 40 * avg_in_nba = 48 * avg_in_fiba
+                    // avg_in_fiba = 40 * avg_in_nba / 48
+                    double randomProbability = Program.RandomNumberGenerator.NextDouble();
+                    Console.WriteLine("Randomly generated                   probability: " + randomProbability);
+                    if (randomProbability.CompareTo(winProbabilityOfTeamWithHigherFIBARanking) <= 0)
+                    {
+                        if (homeTeamHigherRanked)
+                        {
+                            match.Result = new MatchResult(85, 85 - fibaRankingDifference);
+                        }
+                        else
+                        {
+                            match.Result = new MatchResult(85 - fibaRankingDifference, 85);
+                        }
+                    }
+                    else
+                    {
+                        if (homeTeamHigherRanked)
+                        {
+                            match.Result = new MatchResult(85 - fibaRankingDifference, 85);
+                        }
+                        else
+                        {
+                            match.Result = new MatchResult(85, 85 - fibaRankingDifference);
+                        }
+                    }
+                    Console.WriteLine("Match                                     result: " 
+                        + match.Result.HomeTeamPoints + ":" + match.Result.GuestTeamPoints);
+                }
+            }
+        }
     }
 }
