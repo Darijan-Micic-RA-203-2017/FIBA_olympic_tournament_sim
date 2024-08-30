f7188e75bace1589a265531cca13c758421026c7
Darijan Mićić <darijan.micic10@gmail.com>
2024-08-30 20:16:42 +0200

    feat: create match service with utility methods

---
+++ FIBA_OT_sim/FIBA_OT_sim/Services/MatchService.cs
@@ -0,0 +1,52 @@
+﻿using FIBA_OT_sim.Model;
+
+namespace FIBA_OT_sim.Services
+{
+    public class MatchService
+    {
+        public MatchService() { }
+
+        public static NationalTeam GetNationalTeamThatWonMatch(Match match)
+        {
+            NationalTeam nationalTeamThatWonMatch = null;
+            if (match.Result.HomeTeamPoints - match.Result.GuestTeamPoints > 0)
+            {
+                nationalTeamThatWonMatch = match.HomeTeam;
+            }
+            else
+            {
+                nationalTeamThatWonMatch = match.GuestTeam;
+            }
+
+            return nationalTeamThatWonMatch;
+        }
+
+        public static NationalTeam GetNationalTeamThatLostMatch(Match match)
+        {
+            NationalTeam nationalTeamThatLostMatch = null;
+            if (match.Result.HomeTeamPoints - match.Result.GuestTeamPoints < 0)
+            {
+                nationalTeamThatLostMatch = match.HomeTeam;
+            }
+            else
+            {
+                nationalTeamThatLostMatch = match.GuestTeam;
+            }
+
+            return nationalTeamThatLostMatch;
+        }
+
+        public static Match FindMatchBetweenTeams(NationalTeam team1, NationalTeam team2)
+        {
+            foreach (Match match in team1.Matches)
+            {
+                if (match.IsMatchBetweenTeams(team1, team2))
+                {
+                    return match;
+                }
+            }
+
+            return null;
+        }
+    }
+}
