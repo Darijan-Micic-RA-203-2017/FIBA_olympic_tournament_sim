aab3b2484ff73bbbf7c0c738caf6c480e9c1d59d
Darijan Mićić <darijan.micic10@gmail.com>
2024-08-31 03:31:18 +0200

    fix: access static property group phase in test

--- FIBA_OT_sim/FIBA_OT_sim_Tests/LoadGroupPhaseFromFileSystemTests.cs
+++ FIBA_OT_sim/FIBA_OT_sim_Tests/LoadGroupPhaseFromFileSystemTests.cs
@@ -16,56 +16,55 @@ namespace FIBA_OT_sim_Tests
         public void Load_group_phase_from_file_system()
         {
             GroupPhaseRepository groupPhaseRepository = new GroupPhaseRepository();
-
             groupPhaseRepository.LoadGroupPhaseFromFileSystem("../../../../../FIBA_OT_sim/Resources/groups.json");
 
-            Assert.IsNotNull(groupPhaseRepository.GroupPhase);
-            Assert.AreEqual(3, groupPhaseRepository.GroupPhase.Groups.Count);
+            Assert.IsNotNull(GroupPhaseRepository.GroupPhase);
+            Assert.AreEqual(3, GroupPhaseRepository.GroupPhase.Groups.Count);
 
-            Assert.AreEqual("A", groupPhaseRepository.GroupPhase.Groups[0].Name);
-            Assert.AreEqual(4, groupPhaseRepository.GroupPhase.Groups[0].Teams.Count);
-            Assert.AreEqual("Kanada", groupPhaseRepository.GroupPhase.Groups[0].Teams[0].Name);
-            Assert.AreEqual("CAN", groupPhaseRepository.GroupPhase.Groups[0].Teams[0].Abbreviation);
-            Assert.AreEqual(7, groupPhaseRepository.GroupPhase.Groups[0].Teams[0].FIBARanking);
-            Assert.AreEqual("Australija", groupPhaseRepository.GroupPhase.Groups[0].Teams[1].Name);
-            Assert.AreEqual("AUS", groupPhaseRepository.GroupPhase.Groups[0].Teams[1].Abbreviation);
-            Assert.AreEqual(5, groupPhaseRepository.GroupPhase.Groups[0].Teams[1].FIBARanking);
-            Assert.AreEqual("Grčka", groupPhaseRepository.GroupPhase.Groups[0].Teams[2].Name);
-            Assert.AreEqual("GRE", groupPhaseRepository.GroupPhase.Groups[0].Teams[2].Abbreviation);
-            Assert.AreEqual(14, groupPhaseRepository.GroupPhase.Groups[0].Teams[2].FIBARanking);
-            Assert.AreEqual("Španija", groupPhaseRepository.GroupPhase.Groups[0].Teams[3].Name);
-            Assert.AreEqual("ESP", groupPhaseRepository.GroupPhase.Groups[0].Teams[3].Abbreviation);
-            Assert.AreEqual(2, groupPhaseRepository.GroupPhase.Groups[0].Teams[3].FIBARanking);
+            Assert.AreEqual("A", GroupPhaseRepository.GroupPhase.Groups[0].Name);
+            Assert.AreEqual(4, GroupPhaseRepository.GroupPhase.Groups[0].Teams.Count);
+            Assert.AreEqual("Kanada", GroupPhaseRepository.GroupPhase.Groups[0].Teams[0].Name);
+            Assert.AreEqual("CAN", GroupPhaseRepository.GroupPhase.Groups[0].Teams[0].Abbreviation);
+            Assert.AreEqual(7, GroupPhaseRepository.GroupPhase.Groups[0].Teams[0].FIBARanking);
+            Assert.AreEqual("Australija", GroupPhaseRepository.GroupPhase.Groups[0].Teams[1].Name);
+            Assert.AreEqual("AUS", GroupPhaseRepository.GroupPhase.Groups[0].Teams[1].Abbreviation);
+            Assert.AreEqual(5, GroupPhaseRepository.GroupPhase.Groups[0].Teams[1].FIBARanking);
+            Assert.AreEqual("Grčka", GroupPhaseRepository.GroupPhase.Groups[0].Teams[2].Name);
+            Assert.AreEqual("GRE", GroupPhaseRepository.GroupPhase.Groups[0].Teams[2].Abbreviation);
+            Assert.AreEqual(14, GroupPhaseRepository.GroupPhase.Groups[0].Teams[2].FIBARanking);
+            Assert.AreEqual("Španija", GroupPhaseRepository.GroupPhase.Groups[0].Teams[3].Name);
+            Assert.AreEqual("ESP", GroupPhaseRepository.GroupPhase.Groups[0].Teams[3].Abbreviation);
+            Assert.AreEqual(2, GroupPhaseRepository.GroupPhase.Groups[0].Teams[3].FIBARanking);
 
-            Assert.AreEqual("B", groupPhaseRepository.GroupPhase.Groups[1].Name);
-            Assert.AreEqual(4, groupPhaseRepository.GroupPhase.Groups[1].Teams.Count);
-            Assert.AreEqual("Nemačka", groupPhaseRepository.GroupPhase.Groups[1].Teams[0].Name);
-            Assert.AreEqual("GER", groupPhaseRepository.GroupPhase.Groups[1].Teams[0].Abbreviation);
-            Assert.AreEqual(3, groupPhaseRepository.GroupPhase.Groups[1].Teams[0].FIBARanking);
-            Assert.AreEqual("Francuska", groupPhaseRepository.GroupPhase.Groups[1].Teams[1].Name);
-            Assert.AreEqual("FRA", groupPhaseRepository.GroupPhase.Groups[1].Teams[1].Abbreviation);
-            Assert.AreEqual(9, groupPhaseRepository.GroupPhase.Groups[1].Teams[1].FIBARanking);
-            Assert.AreEqual("Brazil", groupPhaseRepository.GroupPhase.Groups[1].Teams[2].Name);
-            Assert.AreEqual("BRA", groupPhaseRepository.GroupPhase.Groups[1].Teams[2].Abbreviation);
-            Assert.AreEqual(12, groupPhaseRepository.GroupPhase.Groups[1].Teams[2].FIBARanking);
-            Assert.AreEqual("Japan", groupPhaseRepository.GroupPhase.Groups[1].Teams[3].Name);
-            Assert.AreEqual("JPN", groupPhaseRepository.GroupPhase.Groups[1].Teams[3].Abbreviation);
-            Assert.AreEqual(26, groupPhaseRepository.GroupPhase.Groups[1].Teams[3].FIBARanking);
+            Assert.AreEqual("B", GroupPhaseRepository.GroupPhase.Groups[1].Name);
+            Assert.AreEqual(4, GroupPhaseRepository.GroupPhase.Groups[1].Teams.Count);
+            Assert.AreEqual("Nemačka", GroupPhaseRepository.GroupPhase.Groups[1].Teams[0].Name);
+            Assert.AreEqual("GER", GroupPhaseRepository.GroupPhase.Groups[1].Teams[0].Abbreviation);
+            Assert.AreEqual(3, GroupPhaseRepository.GroupPhase.Groups[1].Teams[0].FIBARanking);
+            Assert.AreEqual("Francuska", GroupPhaseRepository.GroupPhase.Groups[1].Teams[1].Name);
+            Assert.AreEqual("FRA", GroupPhaseRepository.GroupPhase.Groups[1].Teams[1].Abbreviation);
+            Assert.AreEqual(9, GroupPhaseRepository.GroupPhase.Groups[1].Teams[1].FIBARanking);
+            Assert.AreEqual("Brazil", GroupPhaseRepository.GroupPhase.Groups[1].Teams[2].Name);
+            Assert.AreEqual("BRA", GroupPhaseRepository.GroupPhase.Groups[1].Teams[2].Abbreviation);
+            Assert.AreEqual(12, GroupPhaseRepository.GroupPhase.Groups[1].Teams[2].FIBARanking);
+            Assert.AreEqual("Japan", GroupPhaseRepository.GroupPhase.Groups[1].Teams[3].Name);
+            Assert.AreEqual("JPN", GroupPhaseRepository.GroupPhase.Groups[1].Teams[3].Abbreviation);
+            Assert.AreEqual(26, GroupPhaseRepository.GroupPhase.Groups[1].Teams[3].FIBARanking);
 
-            Assert.AreEqual("C", groupPhaseRepository.GroupPhase.Groups[2].Name);
-            Assert.AreEqual(4, groupPhaseRepository.GroupPhase.Groups[2].Teams.Count);
-            Assert.AreEqual("Sjedinjene Države", groupPhaseRepository.GroupPhase.Groups[2].Teams[0].Name);
-            Assert.AreEqual("USA", groupPhaseRepository.GroupPhase.Groups[2].Teams[0].Abbreviation);
-            Assert.AreEqual(1, groupPhaseRepository.GroupPhase.Groups[2].Teams[0].FIBARanking);
-            Assert.AreEqual("Srbija", groupPhaseRepository.GroupPhase.Groups[2].Teams[1].Name);
-            Assert.AreEqual("SRB", groupPhaseRepository.GroupPhase.Groups[2].Teams[1].Abbreviation);
-            Assert.AreEqual(4, groupPhaseRepository.GroupPhase.Groups[2].Teams[1].FIBARanking);
-            Assert.AreEqual("Južni Sudan", groupPhaseRepository.GroupPhase.Groups[2].Teams[2].Name);
-            Assert.AreEqual("SSD", groupPhaseRepository.GroupPhase.Groups[2].Teams[2].Abbreviation);
-            Assert.AreEqual(34, groupPhaseRepository.GroupPhase.Groups[2].Teams[2].FIBARanking);
-            Assert.AreEqual("Puerto Riko", groupPhaseRepository.GroupPhase.Groups[2].Teams[3].Name);
-            Assert.AreEqual("PRI", groupPhaseRepository.GroupPhase.Groups[2].Teams[3].Abbreviation);
-            Assert.AreEqual(16, groupPhaseRepository.GroupPhase.Groups[2].Teams[3].FIBARanking);
+            Assert.AreEqual("C", GroupPhaseRepository.GroupPhase.Groups[2].Name);
+            Assert.AreEqual(4, GroupPhaseRepository.GroupPhase.Groups[2].Teams.Count);
+            Assert.AreEqual("Sjedinjene Države", GroupPhaseRepository.GroupPhase.Groups[2].Teams[0].Name);
+            Assert.AreEqual("USA", GroupPhaseRepository.GroupPhase.Groups[2].Teams[0].Abbreviation);
+            Assert.AreEqual(1, GroupPhaseRepository.GroupPhase.Groups[2].Teams[0].FIBARanking);
+            Assert.AreEqual("Srbija", GroupPhaseRepository.GroupPhase.Groups[2].Teams[1].Name);
+            Assert.AreEqual("SRB", GroupPhaseRepository.GroupPhase.Groups[2].Teams[1].Abbreviation);
+            Assert.AreEqual(4, GroupPhaseRepository.GroupPhase.Groups[2].Teams[1].FIBARanking);
+            Assert.AreEqual("Južni Sudan", GroupPhaseRepository.GroupPhase.Groups[2].Teams[2].Name);
+            Assert.AreEqual("SSD", GroupPhaseRepository.GroupPhase.Groups[2].Teams[2].Abbreviation);
+            Assert.AreEqual(34, GroupPhaseRepository.GroupPhase.Groups[2].Teams[2].FIBARanking);
+            Assert.AreEqual("Puerto Riko", GroupPhaseRepository.GroupPhase.Groups[2].Teams[3].Name);
+            Assert.AreEqual("PRI", GroupPhaseRepository.GroupPhase.Groups[2].Teams[3].Abbreviation);
+            Assert.AreEqual(16, GroupPhaseRepository.GroupPhase.Groups[2].Teams[3].FIBARanking);
         }
     }
 }
