6916a3c8658c8b185c18a060d209f15cca99442a
Darijan Mićić <darijan.micic10@gmail.com>
2024-09-01 21:24:52 +0200

    improvement: create pot class

---
+++ FIBA_OT_sim/FIBA_OT_sim/Model/PotInDrawForEliminationPhase.cs
@@ -0,0 +1,28 @@
+﻿namespace FIBA_OT_sim.Model
+{
+    public class PotInDrawForEliminationPhase
+    {
+        private IList<NationalTeam> nationalTeams;
+
+        public PotInDrawForEliminationPhase()
+        {
+            nationalTeams = new List<NationalTeam>();
+        }
+
+        public PotInDrawForEliminationPhase(IList<NationalTeam> nationalTeams)
+        {
+            this.nationalTeams = nationalTeams;
+        }
+
+        public PotInDrawForEliminationPhase(PotInDrawForEliminationPhase pot)
+        {
+            nationalTeams = pot.nationalTeams;
+        }
+
+        public IList<NationalTeam> NationalTeams
+        {
+            get { return nationalTeams; }
+            set { nationalTeams = value; }
+        }
+    }
+}
