36ffefba8b77e458cf8b6a3fa0b2c3e852d2f30e
Darijan Mićić <darijan.micic10@gmail.com>
2024-09-01 04:26:54 +0200

    feat: place teams to correct pots in draw service

---
+++ FIBA_OT_sim/FIBA_OT_sim/Services/DrawForEliminationPhaseService.cs
@@ -0,0 +1,50 @@
+﻿using FIBA_OT_sim.Model;
+
+namespace FIBA_OT_sim.Services
+{
+    public class DrawForEliminationPhaseService
+    {
+        private static DrawForEliminationPhase draw;
+
+        public DrawForEliminationPhaseService()
+        {
+            draw = new DrawForEliminationPhase();
+        }
+
+        public static DrawForEliminationPhase Draw
+        {
+            get { return draw; }
+            set { draw = value; }
+        }
+
+        public static void PerformDrawForEliminationPhase()
+        {
+            PlaceNationalTeamsIntoCorrectPots();
+        }
+
+        public static void PlaceNationalTeamsIntoCorrectPots()
+        {
+            IList<NationalTeam> nationalTeamsQualifiedToEliminationPhase = 
+                NationalTeamService.GetNationalTeamsQualifiedToEliminationPhase();
+            foreach (NationalTeam nationalTeam in nationalTeamsQualifiedToEliminationPhase)
+            {
+                if (nationalTeam.GroupPhaseRanking <= 2)
+                {
+                    draw.PotD.Add(nationalTeam);
+                }
+                else if (nationalTeam.GroupPhaseRanking <= 4)
+                {
+                    draw.PotE.Add(nationalTeam);
+                }
+                else if (nationalTeam.GroupPhaseRanking <= 6)
+                {
+                    draw.PotF.Add(nationalTeam);
+                }
+                else
+                {
+                    draw.PotG.Add(nationalTeam);
+                }
+            }
+        }
+    }
+}
